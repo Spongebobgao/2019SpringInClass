503d3b212f72fe1eb61eea26fc92f93bb2be1f68
Spongebobgao <spongebobgao@gmail.com>
2019-03-11 10:59:28 -0400

    install bootstrap

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path')
+const users = require('./controllers/users');//users is a express router/controller
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../NoFramework")));
+app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/users', users);
+
+app.listen(port, () => console.log(`Example app http://localhost:${port}`));
---
+++ server/models/user.js
@@ -0,0 +1,33 @@
+const conn = require('./mysql_connection');//require is part of the common js
+
+const model = {
+    getAll(cb){//cb is a function from controller
+        conn.query("SELECT * FROM 2019Spring_Persons", (err, data) => {
+            cb (err, data[0])
+        });
+    },
+    get(id,cb) {
+        conn.query("SELECT * FROM 2019Spring_Persons Id = ?", id, (err, data) => {
+            cb (err, data)
+        })
+
+    },
+    add(input,cb) {
+        if (input.Password.length<8){
+            cb(Error('A longer Password required.'));
+        }
+        conn.query("INSERT INTO 2019Spring_Persons (FirtName,LastName,Birthday,Password,created_at) VALUES (?)",
+                    [[input.FirstName, input.LastName, input.Birthday, input.Password, new Date()]],
+                    (err, data) => {
+                        if (err) {
+                            cb(err);
+                            return
+                        }
+                        model.get(data.insertID, (err, data) => {
+                            cb (err, data)
+                        })
+    })
+  }
+}
+
+module.exports = model;
